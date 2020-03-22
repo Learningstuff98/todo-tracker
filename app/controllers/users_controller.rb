1152f970d7fbdc3e2cb9bef7e25e9a0d66c8f19c
Andy Strube <andystrube64@gmail.com>
2020-03-21 22:28:01 -0700

    Added search functionality for users and their projects on the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @projects = @user.projects.order("title").page(params[:page]).per_page(2)
-    # Project.order("title").page(params[:page]).per_page(2)
+    @search = params["search"]
+    if @search.present?
+      @title = @search["title"]
+      @projects = Project.where("title ILIKE ?", "%#{@title}%")
+      @projects = @projects.order("title").page(params[:page]).per_page(2)
+    end
   end
      
 end
