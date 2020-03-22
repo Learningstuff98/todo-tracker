c1ee8e2f6b6248c8d48dc7193dcd3f715aad94b6
Andy Strube <andystrube64@gmail.com>
2020-03-21 22:04:31 -0700

    Added pagination for projects on the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @projects = @user.projects.all
+    @projects = @user.projects.order("title").page(params[:page]).per_page(2)
+    # Project.order("title").page(params[:page]).per_page(2)
   end
      
 end
