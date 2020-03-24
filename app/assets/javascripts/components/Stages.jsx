312cb0f815b9c6aa89d2196feb024314cc2ec673
Andy Strube <andystrube64@gmail.com>
2020-03-23 20:09:37 -0700

    Fixed bug on production

--- app/assets/javascripts/components/Stages.jsx
+++ app/assets/javascripts/components/Stages.jsx
@@ -31,7 +31,7 @@ class Stages extends React.Component {
 
   getStages() {
     //axios.get('http://localhost:3000/projects/' + this.props.project_id + '/stages')
-    axios.post('https://todo-tracker-andy-strube.herokuapp.com/projects/' + this.props.project_id + '/stages', formData)
+    axios.post('https://todo-tracker-andy-strube.herokuapp.com/projects/' + this.props.project_id + '/stages')
     .then((res) => this.setStagesInState(res.data))
     .catch((err) => console.log(err.response.data));
   }
@@ -54,9 +54,9 @@ class Stages extends React.Component {
   render() {
     return(
       <div>
-        <form onSubmit={(e) => this.onSubmit(e)} >
+        <form onSubmit={(e) => this.onSubmit(e)}>
           <input type='text' placeholder='Stage Name' ref={(input) => this.stageName = input}/>
-          <input type="submit" value="Create Project Stage" />
+          <input type="submit" value="Create Project Stage"/>
         </form>
         <div>
           {this.state.stages.map((stage) => {
