be35667313c89d50c57fcb50fc6ecc821809079a
Andy Strube <andystrube64@gmail.com>
2020-03-24 15:48:46 -0700

    Added some spacing between stage components

--- app/assets/javascripts/components/Stage.jsx
+++ app/assets/javascripts/components/Stage.jsx
@@ -1,7 +1,10 @@
 class Stage extends React.Component {
   render() {
-    return <span className="stage-box">
-      {this.props.stageName}
-    </span>;
-  }  
+    return <span>
+      <span className="stage-box">
+        {this.props.stageName}
+      </span>
+      <span className="spacer-box"></span>
+    </span>
+  }
 }
