df920556c19186251ca4996b66ee17a0756745cf
Andy Strube <andystrube64@gmail.com>
2020-03-23 18:30:48 -0700

    Added heroku url for the stage component

--- app/assets/javascripts/components/Stage.jsx
+++ app/assets/javascripts/components/Stage.jsx
@@ -9,7 +9,8 @@ class Stage extends React.Component {
     const formData = {
       name: stageName
     };
-    axios.post('http://localhost:3000/projects/' + this.props.project_id + '/stages', formData)
+    // axios.post('http://localhost:3000/projects/' + this.props.project_id + '/stages', formData)
+    axios.post('https://todo-tracker-andy-strube.herokuapp.com/projects/' + this.props.project_id + '/stages', formData)
     .then((res) => console.log(res) )
     .catch((err) => console.log(err.response.data) );
   }
