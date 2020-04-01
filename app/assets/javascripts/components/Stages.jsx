3345386570ae31adf6d62fb1f0a025f77a1d7202
Andy Strube <andystrube64@gmail.com>
2020-04-01 13:49:13 -0700

    Refactored stage functionality

--- app/assets/javascripts/components/Stages.jsx
+++ app/assets/javascripts/components/Stages.jsx
@@ -2,49 +2,21 @@ class Stages extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stages: [],
-      selectedTicket: null,
-      ticketUpdates: 0
+      stages: []
     }
-    this.selectTicket = this.selectTicket.bind(this);
-    this.unselectTicket = this.unselectTicket.bind(this);
   }
 
   componentDidMount() {
     this.getStages();
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-    this.submitStage(this.stageName.value);
-    this.clearInputElement();
-  }
-
-  clearInputElement() {
-    this.stageName.value = '';
-  }
-
-  submitStage(stageName) {
-    const formData = {
-      name: stageName
-    };
-    this.submitStageToAPI(formData);
-  }
-
-  buildURL() {
-    //const root = 'http://localhost:3000';
-    const root = 'https://todo-tracker-andy-strube.herokuapp.com';
-    return root + '/projects/' + this.props.project_id + '/stages';
-  }
-
-  submitStageToAPI(formData) {
-    axios.post(this.buildURL(), formData)
-    .then(() => this.getStages())
-    .catch((err) => console.log(err.response.data));
+  setRoot() {
+    return 'http://localhost:3000';
+    //return 'https://todo-tracker-andy-strube.herokuapp.com';
   }
 
   getStages() {
-    axios.get(this.buildURL())
+    axios.get(this.setRoot() + '/projects/' + this.props.project_id + '/stages')
     .then((res) => this.setStagesInState(res.data))
     .catch((err) => console.log(err.response.data));
   }
@@ -55,60 +27,40 @@ class Stages extends React.Component {
     });
   }
 
-  selectTicket(ticket) {
-    this.setState({
-      selectedTicket: ticket
-    });
-    this.getStages();
-  }
-
-  removeOldTicket(ticket) {
-    //axios.delete('http://localhost:3000/tickets/' + ticket.id)
-    axios.delete('https://todo-tracker-andy-strube.herokuapp.com/tickets/' + ticket.id)
-    .catch((err) => console.log(err.response.data));
-  }
-
-  lookForOldTicket(tickets, selectedTicket) {
-    tickets.forEach((ticket) => {
-      if(ticket.name === selectedTicket.name) {
-        if(ticket.id === selectedTicket.id) {
-          this.removeOldTicket(ticket);
-        }
-      }
-    });
-  }
-
-  handleOldTicket(selectedTicket) {
-    //axios.get('http://localhost:3000/projects/' + this.props.project_id + '/edit')
-    axios.get('https://todo-tracker-andy-strube.herokuapp.com/projects/' + this.props.project_id + '/edit')
-    .then((res) => this.lookForOldTicket(res.data, selectedTicket))
-    .catch((err) => console.log(err.response.data));
-  }
-
-  unselectTicket() {
-    this.handleOldTicket(this.state.selectedTicket);
-    this.setState({
-      selectedTicket: null,
-      ticketUpdates: this.state.ticketUpdates + 1
-    });
-    this.getStages();
-  }
-
   buildStages(res) {
     return res.map((stage) => {
       return(
         <Stage
           stageName={stage.name}
-          project_id={this.props.project_id}
-          stage_id={stage.id}
-          current_user={this.props.current_user}
-          selectTicket={this.selectTicket}
-          selectedTicket={this.state.selectedTicket}
-          unselectTicket={this.unselectTicket}
-          ticketUpdates={this.state.ticketUpdates}
         />
       );
     });
+  }
+
+  renderStages() {
+    return <span className="stages">
+      {this.state.stages.map((stage) => {
+        return stage;
+      })}
+    </span>
+  }
+
+  clearInputElement() {
+    this.stageName.value = '';
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.submitStage({
+      name: this.stageName.value
+    });
+    this.clearInputElement();
+  }
+
+  submitStage(formData) {
+    axios.post(this.setRoot() +  '/projects/' + this.props.project_id + '/stages', formData)
+    .then(() => this.getStages())
+    .catch((err) => console.log(err.response.data));
   }
 
   buildStageForm() {
@@ -120,34 +72,11 @@ class Stages extends React.Component {
     }
   }
 
-  renderStages() {
-    return(
-      <span className="stages">
-        {this.state.stages.map((stage) => {
-          return stage;
-        })}
-      </span>
-    );
-  }
-
-  renderSelectedTicket(selectedTicket) {
-    return(
-      <SelectedTicket
-        selectedTicket={selectedTicket}
-      />
-    );
-  }
-
   render() {
-    return(
-      <div>
-        <br/>
-        {this.buildStageForm()}
-        <br/>
-        {this.renderStages()}
-        <br/>
-        {this.renderSelectedTicket(this.state.selectedTicket)}
-      </div>
-    );
+    return <div>
+      {this.buildStageForm()}
+      <br/><br/>
+      {this.renderStages()}
+    </div>
   }
 }
