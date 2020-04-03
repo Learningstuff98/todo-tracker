ec386648464ae7c835afac8630010df563218d2b
Andy Strube <andystrube64@gmail.com>
2020-04-02 18:05:47 -0700

    Refactored selected ticket component

--- app/assets/javascripts/components/SelectedTicket.jsx
+++ app/assets/javascripts/components/SelectedTicket.jsx
@@ -1,13 +1,15 @@
 class SelectedTicket extends React.Component {
   render() {
-    if(this.props.ticket && this.props.current_user) {
-      return <h3 className='make-it-green'>
-        Selected Ticket:{" "}{this.props.ticket.name}
-      </h3>
-    } else if(this.props.current_user) {
-      return <h3 className="make-it-green">
-        No Selected Tickets
-      </h3>
+    if(this.props.current_user) {
+      if(this.props.ticket) {
+        return <h3 className='make-it-green'>
+          Selected Ticket:{" "}{this.props.ticket.name}
+        </h3>
+      } else {
+        return <h3 className="make-it-green">
+          No Selected Tickets
+        </h3>
+      }
     } else {
       return <div></div>
     }
