40e6f5016e7a64aa5d06b02b6c3cf8d3b8bdb47c
Andy Strube <andystrube64@gmail.com>
2020-04-15 18:59:19 -0700

    Project owners can remove contributors

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,10 +1,20 @@
 class ContributorsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:destroy]
   before_action :authenticate_user!
   
   def create
     project = Project.find(params[:project_id])
     if current_user == project.user
       project.contributors.create(contributor_params)
+      redirect_to project_path(project)
+    end
+  end
+
+  def destroy
+    project = Project.find(params[:project_id])
+    if current_user == project.user 
+      contributor = Contributor.find(params[:id])
+      contributor.destroy
       redirect_to project_path(project)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'projects#index'
   resources :users, only: [:show]
   resources :projects, only: [:new, :create, :show, :edit] do
-    resources :contributors, only: [:create]
+    resources :contributors, only: [:create, :destroy]
     resources :stages, only: [:create, :index, :destroy] do
       resources :tickets, only: [:create]
     end
